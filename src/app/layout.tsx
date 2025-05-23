e527efef6cccf88d4e5cc911b5c075a39f8a563a
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-05-22 21:09:02 -0300

    adds lucide react and customizes font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Gaming List",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body className="bg-neutral-900 text-white">{children}</body>
     </html>
   );
