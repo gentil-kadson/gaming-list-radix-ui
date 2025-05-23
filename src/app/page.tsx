c3349ba6560b17827798299051cad6bf18ff31d0
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-05-22 22:47:40 -0300

    feature: finishes main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
 import GameCard from "@/components/GameCard";
 import Game from "@/types/GameType";
+import { CirclePlus, Gamepad } from "lucide-react";
 
 export default async function Home() {
   const response = await fetch("http://localhost:3001/games");
   const games: Game[] = await response.json();
 
   return (
-    <section className="flex flex-col gap-6">
-      <h1 className="text-3xl font-bold">Games Catalog</h1>
-      {games.map((game) => (
-        <GameCard key={game.id} game={game} />
-      ))}
-    </section>
+    <>
+      <section className="flex flex-col gap-6 items-center">
+        <h1 className="text-3xl font-bold flex items-center justify-center gap-5">
+          <Gamepad size={64} />
+          <span>
+            <span className="text-purple-600">Games</span> Catalog
+          </span>
+        </h1>
+        <button className="bg-purple-600 w-36 p-3 rounded flex items-center justify-around">
+          <CirclePlus />
+          Add Game
+        </button>
+      </section>
+      <div className="max-w-7xl m-auto flex flex-col gap-7 mt-9">
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </div>
+    </>
   );
 }
